a517bf706519b5fa16850b621c54492694fa7c87
Thibault Godouet <yo8192@users.noreply.github.com>
2000-10-26 14:29:59 +0000

    bug corrected : checks for no bit sets in bits array used to check only the cl_mins field

--- conf.c
+++ conf.c
@@ -22,7 +22,7 @@
  *  `LICENSE' that comes with the fcron source distribution.
  */
 
- /* $Id: conf.c,v 1.26 2000-10-08 10:10:17 thib Exp $ */
+ /* $Id: conf.c,v 1.27 2000-10-26 14:29:59 thib Exp $ */
 
 #include "fcron.h"
 
@@ -452,19 +452,19 @@ read_file(const char *file_name, CF *cf)
 		error("No min set in '%s': setting all", cl->cl_shell);
 		bit_nset(cl->cl_mins, 0, 59);
 	    }
-	    if ( memcmp(cl->cl_mins, zero, bitstr_size(24)) == 0 ) {
+	    if ( memcmp(cl->cl_hrs, zero, bitstr_size(24)) == 0 ) {
 		error("No hour set in '%s': setting all", cl->cl_shell);
 		bit_nset(cl->cl_hrs, 0, 23);
 	    }
-	    if ( memcmp(cl->cl_mins, zero, bitstr_size(32)) == 0 ) {
+	    if ( memcmp(cl->cl_days, zero, bitstr_size(32)) == 0 ) {
 		error("No month day set in '%s': setting all", cl->cl_shell);
 		bit_nset(cl->cl_days, 0, 31);
 	    }
-	    if ( memcmp(cl->cl_mins, zero, bitstr_size(12)) == 0 ) {
+	    if ( memcmp(cl->cl_mons, zero, bitstr_size(12)) == 0 ) {
 		error("No month set in '%s': setting all", cl->cl_shell);
 		bit_nset(cl->cl_mons, 0, 11);
 	    }
-	    if ( memcmp(cl->cl_mins, zero, bitstr_size(8)) == 0 ) {
+	    if ( memcmp(cl->cl_dow, zero, bitstr_size(8)) == 0 ) {
 		error("No week day set in '%s': setting all", cl->cl_shell);
 		bit_nset(cl->cl_dow, 0, 7);
 	    }
