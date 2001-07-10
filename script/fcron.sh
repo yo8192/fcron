51f1d96721575111309407652f8f18f7cde15702
Thibault Godouet <yo8192@users.noreply.github.com>
2001-07-10 14:50:09 +0000

    added support for "stop"

--- script/fcron.sh
+++ script/fcron.sh
@@ -7,8 +7,11 @@ case "$1" in
   start)
     $SBIN/fcron -b && echo -n " fcron"
     ;;
+  stop)
+    killall -TERM fcron
+    ;;
   *)
-    echo "Usage: fcron start"
+    echo "Usage: fcron start|stop"
     exit 1
     ;;
 esac
